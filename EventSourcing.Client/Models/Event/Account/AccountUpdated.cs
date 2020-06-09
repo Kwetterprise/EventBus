d834412fe3fbe9126fba8131bba941c1b750efbd
Tim van den Essen <tim.van.den.essen@prodrive-technologies.com>
2020-06-09 12:08:50 +0200

    add actor to tweet delete event

--- EventSourcing.Client/Models/Event/Account/AccountUpdated.cs
+++ EventSourcing.Client/Models/Event/Account/AccountUpdated.cs
@@ -12,12 +12,12 @@ namespace Kwetterprise.EventSourcing.Client.Models.Event.Account
         public AccountUpdated(Guid id, string newUsername, string newEmailAddress, string newHashedPassword, byte[]? newProfilePicture, string newBio)
             : base(EventType.AccountUpdated)
         {
-            Id = id;
-            NewUsername = newUsername;
-            NewEmailAddress = newEmailAddress;
-            NewHashedPassword = newHashedPassword;
-            NewProfilePicture = newProfilePicture;
-            NewBio = newBio;
+            this.Id = id;
+            this.NewUsername = newUsername;
+            this.NewEmailAddress = newEmailAddress;
+            this.NewHashedPassword = newHashedPassword;
+            this.NewProfilePicture = newProfilePicture;
+            this.NewBio = newBio;
         }
 
         public Guid Id { get; set; }
--- EventSourcing.Client/Models/Event/Tweet/TweetDeleted.cs
+++ EventSourcing.Client/Models/Event/Tweet/TweetDeleted.cs
@@ -9,12 +9,15 @@
 
         }
 
-        public TweetDeleted(Guid id)
+        public TweetDeleted(Guid id, Guid actor)
             : base(EventType.TweetDeleted)
         {
             this.Id = id;
+            this.Actor = actor;
         }
 
         public Guid Id { get; set; }
+
+        public Guid Actor { get; set; }
     }
 }
